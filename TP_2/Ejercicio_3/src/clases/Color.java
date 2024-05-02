fc0a7faabe2580d7c63b3863bf904bcef12b4942
FMartine7i <frmmartinetti98@gmail.com>
2024-05-02 11:50:16 -0300

    TP_2 finalizado

--- TP_2/Ejercicio_3/src/clases/Color.java
+++
@@ -1,5 +0,0 @@
-package clases;
-
-public enum Color {
-    VIOLETA, AZUL, VERDE, CIAN, NARANJA, ROJO, AMARILLO,LILA, MAGENTA, GRIS, CELESTE
-}
--- TP_2/Ejercicio_6/src/clases/Consumo.java
+++
@@ -1,5 +0,0 @@
-package clases;
-
-public enum Consumo {
-    A, B, C, D, E, F
-}
